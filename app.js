fe87d3179d9acd3cd2881c5b78916f429585afcc
Ngozi Emeh <zingo1ng@yahoo.com>
2019-04-18 11:18:56 +0100

    budget percentage

--- app.js
+++ app.js
@@ -172,8 +172,14 @@ var UIController = (function(){
         displayBudget: function(obj){
             document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
             document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
             document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            
+
+            if(obj.percentage > 0){
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '--';
+            }
 
         },
 
