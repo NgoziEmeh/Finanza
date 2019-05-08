4c1dbb48d02b2777137112f1c4692cd6eca5d482
Ngozi Emeh <zingo1ng@yahoo.com>
2019-05-08 10:45:56 +0100

    decimal manipulation

--- app.js
+++ app.js
@@ -253,6 +253,27 @@ var UIController = (function(){
             });
 
         },
+        
+        formatNumber: function(num, type){
+            var numSplit, int, dec;
+            /*
+             + or - before the number
+             exactly 2 decimal points
+             comma sepaating the thousands
+            */
+           num = Math.abs(num);
+           num = num.toFixed(2);
+
+           numSplit = num.split('.');
+           
+           int = numSplit[0];
+           if (int.length > 3){
+               int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3,3);
+           }
+
+           dec = numSplit[1];
+           
+        },
 
         getDOMstrings: function(){
             return DOMstrings;
