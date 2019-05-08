8890e7d5ef617f0426fc428ef9d9b5129b5a68bf
Ngozi Emeh <zingo1ng@yahoo.com>
2019-05-08 13:14:00 +0100

    change event

--- app.js
+++ app.js
@@ -187,6 +187,12 @@ var UIController = (function(){
        return (type === 'exp' ? '-' : '+') + '' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback){
+        for (var i = 0; i < list.length; i++){
+            callback(list[i], i);
+        }
+    }
+
     return {
         getInput: function(){
             return{
@@ -266,11 +272,7 @@ var UIController = (function(){
         displayPercentages: function(percentages){
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback){
-                for (var i = 0; i < list.length; i++){
-                    callback(list[i], i);
-                }
-            }
+
 
             nodeListForEach(fields, function(current, index){
                 if (percentages[index] > 0){
@@ -297,6 +299,19 @@ var UIController = (function(){
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ', ' + year;
         },
 
+        changedType: function(){
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur){
+                cur.classList.toggle('red-focus');
+            })  
+            
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+        }, 
+
         getDOMstrings: function(){
             return DOMstrings;
         },
@@ -322,6 +337,8 @@ var controller = (function(budgetCtrl, UICtrl){
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
 
 
